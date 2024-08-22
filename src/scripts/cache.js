fa5fe513dcf71b69348c1f736838c4066c3b3524
Andres Munoz <andrefe91@gmail.com>
2024-08-22 12:29:45 -0500

    Simple cache management with localforage

---
+++ src/scripts/cache.js
@@ -0,0 +1,27 @@
+import localforage from "localforage";
+
+async function setCache(object) {
+    try {
+        await localforage.setItem("shoppingCache", object);
+        console.log("Cache saved");
+    } catch (error) {
+        console.log(`There was an error saving the cache: ${error}`);
+    }
+}
+
+
+async function getCache() {
+    try {
+        const cache = await localforage.getItem("shoppingCache");
+
+        if (!cache) { //In the given case that the cache is empty
+            console.log("Cache is empty");
+            return {};
+        }
+        return cache
+    } catch (error) {
+        console.log(`There was an error getting the cache: ${error}`);
+    }
+}
+
+
