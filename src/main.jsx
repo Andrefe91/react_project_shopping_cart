11ad129904758f5434a743dad007001dafff203a
Andres Munoz <andrefe91@gmail.com>
2024-08-22 18:35:48 -0500

    Basic navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+//Components
+import ErrorPage from './components/error/error-page';
+// import ProductPage from './components/product-page/product-page;'
+// import CartPage from './components/cart-page/cart-page;'
+// import AboutPage from './components/about-page/about-page;'
+// import HomePage from './components/home-page/home-page;'
+// import NotFoundPage from './components/not-found-page/not-found-page;'
+
 import './index.css'
+import Root from './components/root/root'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </StrictMode>,
 )
