da793793ed0a02801a7148f35dd5a4855a8f72f3
Andres Muñoz <andrefe91@gmail.com>
2024-08-28 17:59:17 -0500

    About this component content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,37 @@
-import "./about.css"
+import "./about.css";
 
-export default function About () {
-    return (
-        <>
-            <p>This is the <b>About</b> Component</p>
-        </>
-    )
+export default function About() {
+	return (
+		<>
+			<div className="about-container">
+				<h2>About this</h2>
+
+				<div className="about-description">
+					<p>
+						This is, of course, a simple exercise with the objective of
+						practicing the latest concept from <strong>The Odin Project</strong>
+						. We are using React Router with some routines to fetch, manage and
+						organice an Online Store data with some simple UI.
+					</p>
+
+					<p>
+						Please note that no actual POST request is being made from this
+						application, so no real transaction is being made, nor we are
+						collecting any data from you
+					</p>
+				</div>
+
+				<div className="about-link">
+					<p>
+						This was made with ❤️ for{" "}
+						<a href="https://theodinproject.com">The Odin Project</a>, find it
+						on{" "}
+						<a href="https://github.com/Andrefe91/react_project_shopping_cart">
+							Github
+						</a>
+					</p>
+				</div>
+			</div>
+		</>
+	);
 }
